b2744f1fef9c3fa5d6170aefc328f6aa0bfb749f
Shawn Price <shawnp@gmail.com>
2014-03-23 22:16:17 -0700

    fix port

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var port = process.env.PORT || 3000;
-require('harp').server(__dirname, { port: 3000 });
+require('harp').server(__dirname, { port: port });
 console.log("Harp is running on port %s", port);
